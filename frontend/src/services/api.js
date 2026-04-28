3686e8e03338a0b289983850ca275c867764d326
kadamyash123 <yashkadam@123>
2026-04-29 02:19:20 +0530

    debug api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,7 @@
 import axios from "axios";
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default api;
